ed6b94bcc6e656ae2c4eb4ba903679db980c05cd
Gillian Tuerah <felicia.g.t.tuerah@gdplabs.id>
2026-03-31 10:39:57 +0700

    bump to 0.2.1

--- gen-ai/examples/a2ui/a2ui-nextjs/src/components/chat/A2UIContent.tsx
+++ gen-ai/examples/a2ui/a2ui-nextjs/src/components/chat/A2UIContent.tsx
@@ -5,7 +5,6 @@ import {
   A2UIComponentProps,
   A2UIMessage,
   AllSurfacesRenderer,
-  ComponentRegistry,
   Provider,
   Types,
 } from "glchat-a2ui-react-renderer";
@@ -19,32 +18,12 @@ export function A2UIContent({
 }>) {
   const handleAction = (action: ActionPayload) => {
     onUserAction?.(action);
-  }
-
-
-  console.log(messages);
-
-  const registry = ComponentRegistry.getInstance();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  registry.register('MyNodeType', { component: MyNodeComponent as any });
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // registry.register('Button', { component: ButtonComponent as any });
-  
+  };
   return (
     <Provider messages={messages} onAction={handleAction}>
       {/* <div className="border border-primary p-4 bg-card">test</div> */}
 
       <AllSurfacesRenderer />
     </Provider>
-  )
-}
-
-function MyNodeComponent({ node }: Readonly<A2UIComponentProps<Types.CustomNode>>) {
-  const label = (node.properties as Record<string, unknown>).label as string | undefined;
-  return <div>ini label tambahan kak: {label}</div>;
-}
-
-function ButtonComponent({ node }: Readonly<A2UIComponentProps<Types.Button>>) {
-  const label = (node.properties as Record<string, unknown>).label as string | undefined;
-  return <button>button bikin sendiri: {label}</button>;
+  );
 }
